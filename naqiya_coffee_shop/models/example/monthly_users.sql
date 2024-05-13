cf2c41dce7c21deccf8b151a09d4bc5a98590901
naqiyamoti <nmotiwalla@workingfamilies.org>
2024-05-13 09:57:39 -0400

    fixing models

--- naqiya_coffee_shop/models/example/monthly_users.sql
+++ naqiya_coffee_shop/models/example/monthly_users.sql
@@ -1,7 +1,15 @@
 /* grouping customers by when they signed up */ 
-select
-    date_trunc(first_order_at, month) as signup_month,
-    count(*) as new_customers
-from {{ ref('customers_and_orders') }}
-group by 1
+with customers as (
+    select * from {{ ref('customers') }}
+), 
+
+base as (
+    select
+        date_trunc(first_order_at, month) as signup_month,
+        count(*) as new_customers
+    from customers 
+    group by 1 
+)
+
+select * from final 
  
